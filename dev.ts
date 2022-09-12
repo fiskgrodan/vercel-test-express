166c39dc6e18ced167c190b2211452d5e59e52dd
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-09-13 01:11:07 +0200

    feat: add hello click load

--- dev.ts
+++ dev.ts
@@ -1,6 +1,9 @@
 import app from "./api/";
+import express from "express";
 
 const PORT = 5000;
+
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
