2867715043a0f793fe46c3a401553c5fcdab90c6
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-09-13 01:31:51 +0200

    refactor: use json

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/api", (request, response) => {
   const now = format(new Date(), "yyyy-MM-dd HH:mm:ss");
-  response.send(`Hello World! ${now}`);
+  response.json({ message: "Hello World!", now });
 });
 
 export default app;
