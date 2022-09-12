da313a7d9bf58d0390bc55181db06557832f5c94
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-09-13 01:17:01 +0200

    fix: add time

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ import { format } from "date-fns";
 const app = express();
 
 app.get("/api", (request, response) => {
-  const now = format(new Date(), "yyyy-MM-dd");
+  const now = format(new Date(), "yyyy-MM-dd HH:mm:ss");
   response.send(`Hello World! ${now}`);
 });
 
